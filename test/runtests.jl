6fd68088b03e09664ddcd039aa9afd468c4e7e78
AshtonSBradley <ashton.bradley@gmail.com>
2022-05-23 10:49:54 +1200

    tests work

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using SpecialFunctions
 using Test
 
 ≈(x,y)=isapprox(x,y,rtol=1e-4)
-# @testset "lerch tests" begin include("lerch_tests.jl") end
+@testset "lerch tests" begin include("lerch_tests.jl") end
 @testset "bose tests" begin include("bose_tests.jl") end
-# @testset "fermi tests" begin include("fermi_tests.jl") end
+@testset "fermi tests" begin include("fermi_tests.jl") end
 
